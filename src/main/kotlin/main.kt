0b6e76a755aa9db46e3b0d729875e5e6c0509031
dinhngocup <18520335@gm.uit.edu.vn>
2022-08-04 13:40:28 +0700

    blocking background thread

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,52 +1,29 @@
 import kotlinx.coroutines.*
 
-//fun main() = runBlocking{// this: CoroutineScope
-//
-//    println("Thread ${Thread.currentThread().name} is running for runBlocking")
-//    launch {
-//        println("[1]Thread [${Thread.currentThread().name}] is running ")
-//        delay(1000L)
-//        println("[2]Thread ${Thread.currentThread().name} is running ")
-//        println("World!")
-//    }
-//
-//    launch{
-//        println("[3]Thread ${Thread.currentThread().name} is running ")
-//        delay(2000)
-//        println("[4]Thread ${Thread.currentThread().name} is running ")
-//    }
-////    delay(4000)
-//
-//    println("Hello")
-//}
-
-
-fun main() {
+fun main() = runBlocking {
     println("Main Program starts: ${Thread.currentThread().name}")
-
-
-    runBlocking {
-
-        launch(Dispatchers.Unconfined) {
-            println("[Unconfined] coroutine starts: ${Thread.currentThread().name}")
-            delay(5000)
-            // After delaying, this coroutine is executed in background thread instead of main thread.
-            println("[Unconfined] coroutine ends: ${Thread.currentThread().name}")
-        }
-
-        launch(Dispatchers.IO) {
-            println("[IO] coroutine starts: ${Thread.currentThread().name}")
-            delay(5000)
-            // After delaying, this coroutine is executed in background thread instead of main thread.
-            println("[IO] coroutine ends: ${Thread.currentThread().name}")
-        }
-
-        println("[RunBlocking] coroutine scope starts: ${Thread.currentThread().name}")
-        delay(2000)
-        println("[RunBlocking] coroutine scope ends: ${Thread.currentThread().name}")
+    launch(Dispatchers.IO) {
+        println("[IO-1] coroutine starts: ${Thread.currentThread().name}")
+        blockingThread()
+        // After delaying, this coroutine is executed in background thread instead of main thread.
+        println("[IO-1] coroutine ends: ${Thread.currentThread().name}")
     }
 
-
+    launch(Dispatchers.IO) {
+        println("[IO-2] coroutine starts: ${Thread.currentThread().name}")
+        blockingThread()
+        // After delaying, this coroutine is executed in background thread instead of main thread.
+        println("[IO-2] coroutine ends: ${Thread.currentThread().name}")
+    }
     println("Main Program ends: ${Thread.currentThread().name}")
+    blockingThread()
+
 }
+
+
+ fun blockingThread() {
+//    println("-----BLOCKING: ${Thread.currentThread().name}")
+    Thread.sleep(2000)
+}
+
 
